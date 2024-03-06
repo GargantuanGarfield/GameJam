332547c38aaca1dd498c9ab07c17f7acef88389e
GargantuanGarfield <145381852+GargantuanGarfield@users.noreply.github.com>
2024-03-06 12:10:43 -0500

    starting

---
+++ Starter_code.py
@@ -0,0 +1,16 @@
+#
+# Gavin Mckenzie
+# 3/6/24
+# Starter code
+from colorama import Fore, Back, Style
+import Ascii_artr
+
+print(f"{Ascii_artr.TITLE_CARED}")
+player = input("Enter your name: ")
+print(f"Welcome detective {player}, Let's cut to the {Fore.RED}{Style.BRIGHT}CHASE{Style.RESET_ALL}")
+print(f"\tDetective... Something happened,")
+crime = input("WHat happened?\n\t")
+print("I guess you've already heard the bad news, get down to the crime scene and investigate..")
+yes = input("Go now? (y/n) ").lower()
+if yes == 'n':
+    print("Too bad, you're going")
