c2f9423a3113a98a7eec3658e44714d6fa03df01
Nwwhitton <nwwhitton@my.waketech.edu>
2024-03-06 12:31:41 -0500

    QOL changes

--- Starter_code.py
+++ Starter_code.py
@@ -10,8 +10,8 @@ player = input("Enter your name: ")
 print(f"Welcome detective {player}, Let's cut to the {Fore.RED}{Style.BRIGHT}CHASE{Style.RESET_ALL}")
 print(f"\tDetective... Something happened,")
 
-list_of_crimes = ["The murder?", "The Arson?", "The Drug House?"]
-crime = input(f"WHat happened {list_of_crimes}?\n\t")
+list_of_crimes = ["the murder?", "the arson?", "the drug house?"]
+crime = input(f"WHat happened {list_of_crimes}?\n\t").lower()
 while crime not in list_of_crimes:
     print("That didn't happen...")
     crime = input(f"WHat happened {list_of_crimes}?\n\t")
@@ -20,3 +20,4 @@ print("I guess you've already heard the bad news, get down to the crime scene an
 yes = input("Go now? (y/n) ").lower()
 if yes == 'n':
     print("Too bad, you're going")
+
