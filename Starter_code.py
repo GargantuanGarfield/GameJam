9bed3a6b2a9cc490ed0a026ff78113f1d4e2c701
vpatel00 <vpatel10@my.waketech.edu>
2024-03-06 13:52:15 -0500

    stuff
    hehehehaw

--- Starter_code.py
+++ Starter_code.py
@@ -4,8 +4,10 @@
 # Starter code
 from colorama import Fore, Style
 import Ascii_artr
+from playsound import playsound
 
 print(f"{Ascii_artr.TITLE_CARED}")
+playsound('whistle.mp3')
 player = input("Enter your name: ")
 print(Ascii_artr.CONNER)
 print(f"Welcome detective {player}, Let's cut to the {Fore.RED}{Style.BRIGHT}CHASE{Style.RESET_ALL}")
@@ -23,3 +25,10 @@ print("I guess you've already heard the bad news, get down to the crime scene an
 yes = input("Go now? (y/n) ").lower().strip()
 if yes == 'n':
     print("Didn't ask")
+
+if crime == "the murder?":
+    pass
+elif crime == "the robbery?":
+    pass
+elif crime == "the drug house?":
+    pass
---
+++ drug.py
@@ -0,0 +1,2 @@
+print("You have entered the drug house")
+first_action = input("What will you do: (go upstairs, go into the kitchen, go into the basement)")
