3920f5bcc3579e7bb5062ab82652352ff7e412d0
Nwwhitton <nwwhitton@my.waketech.edu>
2024-03-06 12:58:47 -0500

    thungue

--- Starter_code.py
+++ Starter_code.py
@@ -10,7 +10,11 @@ player = input("Enter your name: ")
 print(Ascii_artr.CONNER)
 print(f"Welcome detective {player}, Let's cut to the {Fore.RED}{Style.BRIGHT}CHASE{Style.RESET_ALL}")
 print(f"\tDetective... We have a problem--")
-crime = input("WHat happened?\n\t")
+list_of_crimes = ["the murder?", "the robbery?", "the drug house?"]
+crime = input(f"What happened {list_of_crimes}?\n\t").lower()
+while crime not in list_of_crimes:
+    print("That didn't happen...")
+    crime = input(f"WHat happened {list_of_crimes}?\n\t")
 print("I guess you've already heard the bad news, get down to the crime scene and investigate..")
 yes = input("Go now? (y/n) ").lower().strip()
 if yes == 'n':
