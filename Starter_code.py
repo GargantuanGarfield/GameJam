b14101e3926b732dcc8743458a74172f0c53132c
Nwwhitton <nwwhitton@my.waketech.edu>
2024-03-06 12:21:47 -0500

    Added list of crimes and the inputs.

--- Starter_code.py
+++ Starter_code.py
@@ -9,7 +9,13 @@ print(f"{Ascii_artr.TITLE_CARED}")
 player = input("Enter your name: ")
 print(f"Welcome detective {player}, Let's cut to the {Fore.RED}{Style.BRIGHT}CHASE{Style.RESET_ALL}")
 print(f"\tDetective... Something happened,")
-crime = input("WHat happened?\n\t")
+
+list_of_crimes = ["The murder?", "The Arson?", "The Drug House?"]
+crime = input(f"WHat happened {list_of_crimes}?\n\t")
+while crime not in list_of_crimes:
+    print("That didn't happen...")
+    crime = input(f"WHat happened {list_of_crimes}?\n\t")
+
 print("I guess you've already heard the bad news, get down to the crime scene and investigate..")
 yes = input("Go now? (y/n) ").lower()
 if yes == 'n':
