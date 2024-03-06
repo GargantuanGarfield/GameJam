75b2f857a46704040e31086868423e870e2622bc
GargantuanGarfield <145381852+GargantuanGarfield@users.noreply.github.com>
2024-03-06 12:40:40 -0500

    balls

--- Starter_code.py
+++ Starter_code.py
@@ -7,10 +7,11 @@ import Ascii_artr
 
 print(f"{Ascii_artr.TITLE_CARED}")
 player = input("Enter your name: ")
+print(Ascii_artr.CONNER)
 print(f"Welcome detective {player}, Let's cut to the {Fore.RED}{Style.BRIGHT}CHASE{Style.RESET_ALL}")
-print(f"\tDetective... Something happened,")
+print(f"\tDetective... We have a problem--")
 crime = input("WHat happened?\n\t")
 print("I guess you've already heard the bad news, get down to the crime scene and investigate..")
-yes = input("Go now? (y/n) ").lower()
+yes = input("Go now? (y/n) ").lower().strip()
 if yes == 'n':
-    print("Too bad, you're going")
+    print("Didn't ask")
