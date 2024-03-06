5bcf1b87db27f9dbb381d276d20c8de7159f0611
GargantuanGarfield <145381852+GargantuanGarfield@users.noreply.github.com>
2024-03-06 13:05:05 -0500

    havbsdkjjg

--- Starter_code.py
+++ Starter_code.py
@@ -10,14 +10,12 @@ player = input("Enter your name: ")
 print(Ascii_artr.CONNER)
 print(f"Welcome detective {player}, Let's cut to the {Fore.RED}{Style.BRIGHT}CHASE{Style.RESET_ALL}")
 
-print(f"\tDetective... Something happened,")
+print(f"\tDetective... We have a problem--")
 list_of_crimes = ["the murder?", "the robbery?", "the drug house?"]
 crime = input(f"WHat happened {list_of_crimes}?\n\t").lower()
 while crime not in list_of_crimes:
     print("That didn't happen...")
     crime = input(f"WHat happened {list_of_crimes}?\n\t")
-
-print(f"\tDetective... We have a problem--")
 crime = input("What happened?\n\t")
 print("I guess you've already heard the bad news, get down to the crime scene and investigate..")
 yes = input("Go now? (y/n) ").lower().strip()
