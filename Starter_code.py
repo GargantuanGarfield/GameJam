6235d9821b93ef5797385cd94aae1bf53afa117a
Nwwhitton <nwwhitton@my.waketech.edu>
2024-03-07 13:31:23 -0500

    Committing MainFloor to Nicholas Branch

--- Starter_code.py
+++ Starter_code.py
@@ -2,20 +2,33 @@
 # Gavin Mckenzie
 # 3/6/24
 # Starter code
-from colorama import Fore, Back, Style
+from colorama import Fore, Style
 import Ascii_artr
+from os import system
+from time import sleep
+import MainFloor
 
-print(f"{Ascii_artr.TITLE_CARED}")
-player = input("Enter your name: ")
-print(Ascii_artr.CONNER)
-print(f"Welcome detective {player}, Let's cut to the {Fore.RED}{Style.BRIGHT}CHASE{Style.RESET_ALL}")
-print(f"\tDetective... We have a problem--")
-list_of_crimes = ["the murder?", "the robbery?", "the drug house?"]
-crime = input(f"What happened {list_of_crimes}?\n\t").lower()
-while crime not in list_of_crimes:
-    print("That didn't happen...")
-    crime = input(f"WHat happened {list_of_crimes}?\n\t")
-print("I guess you've already heard the bad news, get down to the crime scene and investigate..")
-yes = input("Go now? (y/n) ").lower().strip()
-if yes == 'n':
-    print("Didn't ask")
+def main():
+    print(f"{Ascii_artr.TITLE_CARED}")
+    player = input("Enter your last name: ")
+    for line in Ascii_artr.CONNER.splitlines():
+        print(line)
+        sleep(.086)
+    print(f"Welcome detective {player}, Let's cut to the {Fore.RED}{Style.BRIGHT}CHASE{Style.RESET_ALL}")
+    print(f"\tDetective... We have a problem--")
+    input(f"What happened? \n\t").lower()
+    print(f"\n{Style.BRIGHT}What are you talking about? No there's been a string of murders!{Style.RESET_ALL} Now is not the time for jokes..\n\t\
+    We found where the killer has been hiding out, so go investigate.")
+    print()
+    yes = input("Go now?\n\t(y/n): ").lower().strip()
+    while yes != 'y':
+        print("Try again")
+        yes = input("Go now?]\n\t(y/n): ").lower().strip()
+    print("Wonderful\n")
+    system("cls")
+    for line in Ascii_artr.DOTS:
+        print(line)
+        sleep(.8)
+    MainFloor.setting(player)
+
+main()
