1acceb97f4b56935c42630d4bdd208512fce22bd
Nwwhitton <nwwhitton@my.waketech.edu>
2024-03-08 13:00:40 -0500

    Linked MainFloor With Basement

--- MainFloor.py
+++ MainFloor.py
@@ -1,15 +1,16 @@
 # Nicholas Whitton
 # 3/7/2024
 # The main floor where you search for items.
-
+import basement
+import time
+import random
 selection = ['Basement', 'Upstairs', 'Cabinet', 'Kitchen', 'Check' ]
 cabinet_selection = ['[P]owder???', '[T]ape Recorder', '[W]hetstone']
 kitchen_selection = ['[ID] Body', '[R]iddle??', 'Egg']
 HasKey = False
-import time
-import random
+
+
 # import top_floor
-# import basement
 
 
 def background():
@@ -17,28 +18,39 @@ def background():
 It is a run down house with broken glass and spots of blood everywhere.
 You enter the building, try looking for evidence to solve this case.
 """
+
+
 print(background.__doc__)
-passnum1 = random.randint(0,9)
+passnum1 = random.randint(0, 9)
+
 
 def setting(inventory):
-    inventory = []
+    BASEMENT = False
     global HasKey
     option = input(f'Choose what you want to do {selection}: ').capitalize()
+    if 'Working Tape Recorder' in inventory:
+        print('Great job'), print('.', end=''), time.sleep(1), print('.', end=''), time.sleep(1), print('.'), time.sleep(1)
+        print('You can go home now...')
+        quit()
+    else:
+        pass
     while option not in selection:
         option = input(f'Choose what you want to do {selection}: ').capitalize()
-    while not HasKey:
-        if option == 'Basement' and not HasKey:
-            print('\n You dont have the Key for that')
+    while not BASEMENT:
+        if option == 'Basement' and HasKey == False:
+            print('\nThere is a lock with 4 digits here.')
             passcode = input('What is the passcode? ')
             if passcode == str(passnum1) + '298':
+                print('You unlocked the basement')
                 HasKey = True
             else:
                 print('Wrong code.')
             option = input(f'Choose what you want to do {selection}: ').capitalize()
 
         elif option == 'Basement' and HasKey == True:
-            option = input(f'Choose what you want to do {selection}: ').capitalize()
-            pass # will be basement.basement()
+            print('Moving to basement')
+            BASEMENT = True
+            basement.basement(inventory)
 
         elif option == 'Upstairs':
             quit()
@@ -57,7 +69,7 @@ def setting(inventory):
 
             elif look_for_stuff == ('W' or 'Whetstone'):
                 print('Why are you looking at the Whetstone???')
-                print('.', end=''), time.sleep(2), print('.', end=''), time.sleep(2), print('.'), time.sleep(2)
+                print('.', end=''), time.sleep(1), print('.', end=''), time.sleep(1), print('.'), time.sleep(1)
                 print('Wait!'), time.sleep(1)
                 print(f'There are the numbers [1:{passnum1}]')
 
@@ -74,13 +86,13 @@ def setting(inventory):
                 print("Can't do that in the cabinet!")
 
         elif option == 'Kitchen':
+            print('In the corner of the room there is a body!')
             look_in_kitchen = input(f'Look in cabinet {kitchen_selection} (Q to leave): ')
             if look_in_kitchen == 'Q':
                 option = input(f'Choose what you want to do {selection}: ').capitalize()
-            elif look_in_kitchen == ('B' or 'Bloody Knife'):
-                print('You look at the dried blood on the knife.')
-                print('This may be useful later.'), time.sleep(1), print('You put the knife in your bag.')
-                inventory.append('Bloody Knife')
+
+            elif look_in_kitchen == ('ID' or 'ID Body'):
+                print("Hmm you can't seem to identify the name on the body.")
 
             elif look_in_kitchen == ('R' or 'Riddle'):
                 print('You have 1 MOUTH, and you should LISTEN to me TWICE as much as you are talking. HOW MANY EARS DO YOU HAVE?')
@@ -88,9 +100,9 @@ def setting(inventory):
                 answer = input('How many ears do you have')
                 while answer != '2':
                     print("That doesn't seem like the right answer")
-                    answer = input('How many ears do you have')
+                    answer = input('How many ears do you have? ')
                 else:
-                    print('This may be bart of a passcode of some kind [2:2]')
+                    print('This may be part of a passcode of some kind [2:2]')
 
 
             elif look_in_kitchen == 'Egg':
@@ -98,9 +110,9 @@ def setting(inventory):
                 print('This may be useful')
                 inventory.append('Egg')
         elif option == 'Check':
-            print('You have: ', end=' ')
+            print('You have: ')
             for i in inventory:
-                print(i)
+                print('-'+i)
             print('in your inventory.')
             option = input(f'Choose what you want to do {selection}: ').capitalize()
         else:
